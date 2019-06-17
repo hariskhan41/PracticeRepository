bfe7ae2d8550f46e3dd42c058c90c6e527cb832f
hariskhan41 <hariskhan3465@gmail.com>
2019-06-17 23:33:45 +0500

    2nd Commit

--- PracticeGithub/PracticeGithub/Controllers/HomeController.cs
+++ PracticeGithub/PracticeGithub/Controllers/HomeController.cs
@@ -8,6 +8,18 @@ namespace PracticeGithub.Controllers
 {
     public class HomeController : Controller
     {
+
+        public void HarisFunction1()
+        {
+            
+            //Task 1.
+            
+            for(int i = 0; i < 10; i++)
+            {
+                //something
+            }
+        }
+
         public ActionResult Index()
         {
             return View();
